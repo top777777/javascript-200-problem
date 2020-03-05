8b3fa16871e5f960a2d2f7a45d8ce75801a7b059
SANG BEOM KIM <top777777@gmail.com>
2020-03-05 21:38:03 +0900

    20200305 - stydyAtHome

---
+++ part2/037.js
@@ -0,0 +1,5 @@
+function drawChart(width = 200, height = 400) {
+  console.log(`${width} X ${height} 차트를 그립니다.`);
+}
+drawChart(100);
+drawChart();
---
+++ part2/039.js
@@ -0,0 +1,13 @@
+let a = 10;
+console.log(a);
+
+function print() {
+  let b = 20;
+  if (true) {
+    var c = 30;
+  }
+  console.log(c);
+}
+
+print();
+console.log(b);
